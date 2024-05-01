ad45d69f1fbfe69e8ea7c514e991371e1c5aa70a
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-01 13:35:23 -0500

    Edits

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,3 +1,12 @@
 using System;
 
-Console.WriteLine("Programming is like building a multilingual puzzle");
+namespace _3_writeline
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
