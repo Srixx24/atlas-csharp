0dc8606dd5dc7e865c261b92e95946a205078b04
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-01 14:59:23 -0500

    Edits

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: {0:P2}\nCurrency: {1:C}", percent, currency");
+	}
+}
