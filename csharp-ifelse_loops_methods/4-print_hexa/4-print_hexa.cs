5f387eeba53ee8b80287e27367cd694340628685
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-01 20:31:41 -0500

    Edits

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int x = 0; x <= 98; x++)
+        {    
+            Console.Write("{0} = 0x{0:x}\n",x);
+        }
+    }
+}
