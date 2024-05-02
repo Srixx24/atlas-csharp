1c5627b320ca868f19fa3781380e656676743ecd
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-01 19:01:14 -0500

    Edits

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string alph = "abcdefghijklmnopqrstuvwxyz";
+        string result = ""
+
+        for (int x = 0; x < alph.Length; x++)
+        {
+            result += alph[x]
+        }
+        Console.WriteLine(result);
+    }
+}
