827b392c71bde11fc5211652eed14e9824016fc8
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-02 14:58:53 -0500

    Edits

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,6 +4,12 @@ class Matrix
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        boop
+        if(index < 0 || index > array.Length - 1)
+        {
+            Console.WriteLine("Index out of range");
+            return array;
+        }
+        array[index] = n;
+        return array;
     }
 }
