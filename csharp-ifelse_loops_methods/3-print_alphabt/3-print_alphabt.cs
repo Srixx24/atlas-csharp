2a437a37ed96107acc3aa20106938bc510102824
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-01 20:23:24 -0500

    Edits

--- csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -4,14 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        string alph = "abcdefghijklmnopqrstuvwxyz";
+        string alph = "abcdfghijklmnoprstuvwxyz";
 
         for (int x = 0; x < alph.Length; x++)
         {
-            if (!(x == 'q' || x == 'e'))
-            {
-                Console.Write(alph[x]);
-            }
+            Console.Write(alph[x]);
         }
     }
 }
