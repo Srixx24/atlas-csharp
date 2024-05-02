5bf5ceb3b3df23789613a0f443fd7b65d03693b5
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-01 19:03:48 -0500

    Edits

--- csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
         string alph = "abcdefghijklmnopqrstuvwxyz";
-        string result = ""
+        string result = "";
 
         for (int x = 0; x < alph.Length; x++)
         {
-            result += alph[x]
+            result += alph[x];
         }
         Console.WriteLine(result);
     }
