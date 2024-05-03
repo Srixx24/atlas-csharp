16c979c39f2f73dad8f8db2fdce94f775f36598e
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-02 22:32:09 -0500

    Edits

--- csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
+++ csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 class Dictionary
 {
     public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
     {
-        boop
+        myDict.Remove(key);
+        return myDict;
     }
 }
