1df68f6cc3ac8878d503991cba58f197490e0803
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-03 08:57:56 -0500

    Edits

--- csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Collections.Generic;
 
 class Dictionary
 {
     public static void PrintSorted(Dictionary<string, string> myDict)
     {
-        boop
+        List<string> keyList = new List<string>(myDict.Keys);
+        keyList.Sort();
+
+        foreach (string key in keyList)
+        {
+            Console.WriteLine($"{key}: {myDict[key]}");
+        }
     }
 }
