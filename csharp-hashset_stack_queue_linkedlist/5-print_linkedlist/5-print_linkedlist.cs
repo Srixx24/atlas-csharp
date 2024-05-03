192dcac832b7fc58af626cd4879908ac8b4e0c7e
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-03 18:50:08 -0500

    Edits

--- csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -7,7 +7,7 @@ class LList
     public static LinkedList<int> CreatePrint(int size)
     {
         List<int> list = new List<int>();
-        for (int x = 0; x > size; x++)
+        for (int x = 0; x < size; x++)
         {
             list.Add(x);
             Console.WriteLine(x);
