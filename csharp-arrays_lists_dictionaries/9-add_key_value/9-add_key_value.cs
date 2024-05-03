14620e39672e769b6c8c3c6e5eae29836b2ba1b9
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-02 22:14:54 -0500

    Edits

--- csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 class Dictionary
 {
     public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
     {
-        boop
+        myDict[key] = value;
+        return myDict;
     }
 }
