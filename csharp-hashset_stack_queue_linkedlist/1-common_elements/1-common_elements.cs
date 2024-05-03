f56d24ec65bd1920f0510c070512dea601ac5b11
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-03 12:02:32 -0500

    Edits

--- csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -5,6 +5,18 @@ class List
 {
     public static List<int> CommonElements(List<int> list1, List<int> list2)
     {
-        boop
+        int sortedList = new List<int>();
+
+        foreach (int x in list1)
+        {
+            foreach (int y in list2)
+            {         
+                if (x == y)
+                {
+                    sortedList.Add(x)
+                }
+            }
+        }
+        return(sortedList);
     }
 }
