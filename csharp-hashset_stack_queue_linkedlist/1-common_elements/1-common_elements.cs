ec306c94cdb05a91176758277c64416bb8c2bb20
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-03 12:18:55 -0500

    Edits

--- csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -13,7 +13,7 @@ class List
             {         
                 if (x == y)
                 {
-                    sortedList.Add(x)
+                    sortedList.Add(x);
                 }
             }
         }
