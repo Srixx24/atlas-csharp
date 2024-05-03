96657aed62e18b390e3c60df20452ae8de20cbd5
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-03 13:18:47 -0500

    Edits

--- csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -5,6 +5,24 @@ class List
 {
     public static List<int> DifferentElements(List<int> list1, List<int> list2)
     {
-        boop
+        List<int> singleDidget = new List<int>();
+
+        foreach (int numb in list1)
+        {
+            if (!list2.Contains(numb))
+            {
+                singleDidget.Add(numb);
+            }
+        }
+
+        foreach (int numb in list2)
+        {
+            if (!list1.Contains(numb))
+            {
+                singleDidget.Add(numb);
+            }
+        }
+
+        return singleDidget;
     }
 }
