8546f365e4abd3074df2b7972be703b28246a4be
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-03 19:15:38 -0500

    Edits

--- csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -5,6 +5,13 @@ class LList
 {
     public static int Length(LinkedList<int> myLList)
     {
-        boop
+        int elements = 0;
+
+        foreach (int val in myLList)
+        {
+            elements++;
+            Console.WriteLine();
+        }
+        return elements;
     }
 }
