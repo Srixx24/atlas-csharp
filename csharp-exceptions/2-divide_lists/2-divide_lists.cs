0e3772c4c38d0380e173704cf5e50ed6faf9b314
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-05 12:23:25 -0500

    Edits

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -16,6 +16,7 @@ class List
             catch (DivideByZeroException)
             {
                 Console.WriteLine("Cannot divide by zero");
+                results.Add(0);
             }
             catch (ArgumentOutOfRangeException)
             {
