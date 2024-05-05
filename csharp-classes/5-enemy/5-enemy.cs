13a85b411bae11866ee58158445c7a7cacbb2aba
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-05 14:47:13 -0500

    Edits

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary> An empty zombie class </summary>
+    class Zombie
+    {
+        /// <summary> Zombie health </summary>
+        public int health;
+
+        /// <summary> Set the value of health to 0 </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary> Set health requirement </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        /// <summary> Returns Zombie health </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        /// <summary> Set base name </summary>
+        private string name = "(No name)";
+
+        /// <summary> Name parameters </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary> Overrides Zombie attributes </summary>
+        public string ToString()
+        {
+            return $"Zombie name:{Name} / Total health: {health}";
+        }
+    }
+}
