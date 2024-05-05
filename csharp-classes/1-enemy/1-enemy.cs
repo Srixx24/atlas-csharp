9b30d4c6c467a446e277f59aca54891142e804aa
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-05 13:49:36 -0500

    Edits

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary> An empty zombie class </summary>
+    class Zombie
+    {
+        /// <summary> Zombie health </summary>
+        public int health;
+
+        /// <summary> Set the value of health to 0 </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
