8eddf0ec6b644bb721e152c56025a5ec39efe82f
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-05 10:26:28 -0500

    Edits

--- csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -5,6 +5,18 @@ class List
 {
     public static int SafePrint(List<int> myList, int n)
     {
-        boop
+        int element = 0;
+
+        for (int x = 0; x < n; x++)
+        {
+            try
+            {
+                Console.WriteLine(myList[element])
+            }
+            catch
+            {
+                return element;
+            }
+        }
     }
 }
