4d3ae51e5e49633f50a139daec6265e7f9474a15
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-05 13:41:43 -0500

    Edits

--- csharp-classes/0-enemy/0-enemy.cs
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -2,9 +2,7 @@ using System;
 
 namespace Enemies
 {
-    /// <summary>
-    /// An empty zombie class
-    /// </summary>
+    /// <summary> An empty zombie class </summary>
     class Zombie
     {
 
