5f00b51a679ef323252265375bb198115a967a0d
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-05 14:15:42 -0500

    Edits

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary> An empty zombie class </summary>
+    class Zombie
+    {
+        /// <summary> Zombie health </summary>
+        public int health;
+
+        /// <summary> Set the value of health to 0 </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary> Set health requirement </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        /// <summary> Returns Zombie health </summary>
+        public GetHealth()
+        {
+            return health;
+        }
+    }
+}
