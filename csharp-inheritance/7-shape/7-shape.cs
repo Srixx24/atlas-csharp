590312f7e46e65d54b24cbdf511e33404535fa6f
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-12 11:31:38 -0500

    Edits

--- csharp-inheritance/7-shape/7-shape.cs
+++ csharp-inheritance/7-shape/7-shape.cs
@@ -1,10 +1,56 @@
 using System;
 using System.Collections.Generic;
 
-class Rectangle
+class Shape
 {
-    static Void Main()
+    public virtual int Area()
     {
-        boop
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get {
+            return width;
+        }
+
+        set {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+
+    public int Height
+    {
+        get {
+            return height;
+        }
+
+        set {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+    
+    public new int Area()
+    {
+        return Height * Width
+    }
+
+    public override string ToString()
+    {
+        return $"[Rectangle] {Width} / {Height}";
     }
 }
