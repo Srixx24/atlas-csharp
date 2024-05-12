4c14d9a929fddc0153bec481e94b67e8449dc064
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-12 12:43:17 -0500

    Edits

--- csharp-inheritance/8-shape/8-shape.cs
+++ csharp-inheritance/8-shape/8-shape.cs
@@ -1,10 +1,76 @@
 using System;
 using System.Collections.Generic;
 
-class Square
+class Shape
 {
-    static Void Main()
+    public virtual int Area()
     {
-        boop
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get {
+            return width;
+        }
+
+        set {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+
+    public int Height
+    {
+        get {
+            return height;
+        }
+
+        set {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+    
+    public new int Area()
+    {
+        return Height * Width;
+    }
+
+    public override string ToString()
+    {
+        return $"[Rectangle] {Width} / {Height}";
+    }
+}
+
+class Square : Rectangle
+{
+    private int size
+
+    public int Size
+    {
+        get {
+            return size;
+        }
+
+        set {
+            if (value < 0)
+            {
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            }
+            size = value;
+        }
     }
 }
