f8f7c2a062e13f2eb77fd3f78b3d7a511bd30efc
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-13 21:41:49 -0500

    Edits

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,41 @@
+using System;
+
+abstract class Base
+{
+    public string name = "";
+
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    } 
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class Door : Base, IInteractive
+{
+    public door(string name)
+    {
+        name = "Door"
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the {0}. It's locked.", name);
+    }
+}
