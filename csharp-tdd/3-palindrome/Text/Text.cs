8d088897dbaccf61971a4fb5a1eae15f3a94aa1f
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-15 13:46:05 -0500

    Edits

--- csharp-tdd/3-palindrome/Text/Text.cs
+++ csharp-tdd/3-palindrome/Text/Text.cs
@@ -2,7 +2,16 @@
 
 namespace MyMath
 {
-    public class Class1
+    public class Str
     {
+        public static bool IsPalindrome(string s)
+        {
+            string str2 = s.ToLower();
+            str2 = Regex.Replace(str2, "[:;,. \t\n]", "");
+
+            string reverse = new string(str2.Reverse().ToArray())
+
+            return str2 == reverse;
+        }
     }
 }
