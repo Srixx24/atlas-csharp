1bcf1275f71c51cd340d8ed4f9440eb73cb0f2f8
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-15 14:51:18 -0500

    Edits

--- csharp-tdd/1-divide/MyMath/MyMath.cs
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -5,28 +5,28 @@ namespace MyMath
     public static class Matrix
     {
          
-        //public static int[,] Divide(int[,] matrix, int num)
-        //{
-        //    if (matrix == null)
-        //    {
-        //        return null;
-        //    }
-        //    else if (num == 0)
-        //    {
-        //        Console.WriteLine("Num cannot be 0");
-        //        return null;
-        //    }
-        //    else
-        //    {
-        //        for (int x = 0; x < matrix.GetLength(0); x++)
-        //        {
-        //            for (int y = 0; y < matrix.GetLength(0); y++)
-        //            {
-        //                matrix[x,y] = matrix[x,y] / num;
-        //            }
-        //            return(matrix);
-        //        }
-        //    }
-        //}
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (matrix == null)
+            {
+                return null;
+            }
+            else if (num == 0)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            else
+            {
+                for (int x = 0; x < matrix.GetLength(0); x++)
+                {
+                    for (int y = 0; y < matrix.GetLength(0); y++)
+                    {
+                        matrix[x,y] = matrix[x,y] / num;
+                    }
+                    return(matrix);
+                }
+            }
+        }
     }
 }
