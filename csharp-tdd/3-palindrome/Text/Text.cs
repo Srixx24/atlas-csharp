b3009c461526750a3c8db6754fc4e6850ec9f5a0
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-15 18:29:33 -0500

    Edits

--- csharp-tdd/3-palindrome/Text/Text.cs
+++ csharp-tdd/3-palindrome/Text/Text.cs
@@ -2,16 +2,9 @@
 
 namespace MyMath
 {
-    /// <summary> Provides utility methods for working with strings.</summary>
+
     public class Str
     {
-        /// <summary>
-        /// Determines whether the specified string is a palindrome.
-        /// </summary>
-        /// <param name="s">The input string to be checked.</param>
-        /// <returns>
-        /// <c>true</c> if the input string is a palindrome; otherwise, <c>false</c>.
-        /// </returns>
         //public static bool IsPalindrome(string s)
         //{
         //    // Convert the input string to lowercase
