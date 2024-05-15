5c92f568a9aafa115f8442baaf8014b4aa07d4b2
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-15 16:22:52 -0500

    Edits

--- csharp-tdd/3-palindrome/Text/Text.cs
+++ csharp-tdd/3-palindrome/Text/Text.cs
@@ -2,15 +2,28 @@
 
 namespace MyMath
 {
+    /// <summary> Provides utility methods for working with strings.</summary>
     public class Str
     {
+        /// <summary>
+        /// Determines whether the specified string is a palindrome.
+        /// </summary>
+        /// <param name="s">The input string to be checked.</param>
+        /// <returns>
+        /// <c>true</c> if the input string is a palindrome; otherwise, <c>false</c>.
+        /// </returns>
         public static bool IsPalindrome(string s)
         {
+            // Convert the input string to lowercase
             string str2 = s.ToLower();
+
+            // Remove any punctuation, whitespace, and other non-alphanumeric characters from the string
             str2 = Regex.Replace(str2, "[:;,. \t\n]", "");
 
+            // Reverse the cleaned string
             string reverse = new string(str2.Reverse().ToArray())
 
+            // Compare the cleaned input string with the reversed string
             return str2 == reverse;
         }
     }
