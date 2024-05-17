966a4d5c02d01a27d3adf277856ea5147edb7a5d
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-17 11:54:41 -0500

    Edits

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,41 @@
+using System;
+
+class Queue<T>
+{
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    public class Node
+    {
+        public T Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    public Node head;
+    public Node tail;
+    public count;
+
+    public void Enqueue()
+    {
+        if (tail == null)
+        {
+            head = tail;
+            tail = newNode;
+        }
+
+        count++;
+    }
+
+    public void Count()
+    {
+        return count;
+    }
+}
