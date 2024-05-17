7daefb4f96d10d41a637a633e6c0bf55efc0539a
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-17 16:50:59 -0500

    Edits

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Player
+{
+    public Player(string name, float maxHp)
+    {
+        boop
+    }
+
+    public void PrintHealth()
+    {
+        boop
+    }
+}
---
+++ csharp-generics/5-concatenate/queue.cs
@@ -0,0 +1,99 @@
+using System;
+
+class Queue<T>
+{
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    public class Node
+    {
+        public T Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    public Node head;
+    public Node tail;
+    public int count;
+
+    public void Enqueue(T item)
+    {
+        Node newNode = new Node(item);
+
+        if (tail == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.Next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else
+        {
+            T value = head.Value;
+            head = head.Next;
+            count--;
+
+            if (head == null)
+            {
+                tail = null;
+            }
+
+            return value;
+        }
+    }
+
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else
+        {
+            return head.Value;
+        }
+    }
+
+    public void Print()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        
+        Node cur = head;
+
+        while (cur != null)
+        {
+            Console.WriteLine(cur.Value);
+            cur = cur.Next;
+        }
+    }
+}
