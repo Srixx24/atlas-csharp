07536767463e4941d17d869ab8172f88fe9198f9
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-17 15:32:02 -0500

    Edits

--- csharp-generics/3-peek/queue.cs
+++ csharp-generics/3-peek/queue.cs
@@ -73,7 +73,7 @@ class Queue<T>
         if (head == null)
         {
             Console.WriteLine("Queue is empty");
-            return default(T)
+            return default(T);
         }
         else
         {
