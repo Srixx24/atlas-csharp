8a3da5a475ddfee71946cbac42754fd769fc1096
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-19 17:23:02 -0500

    Edits

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -16,7 +16,7 @@ public class VectorMath
             {
                 vector1[0] + vector2[0],
                 vector1[1] + vector2[1]
-            }
+            };
         }
         else if (vector1.Length == 3 && vector2.Length == 3)
         {
@@ -25,7 +25,7 @@ public class VectorMath
                 vector1[0] + vector2[0],
                 vector1[1] + vector2[1],
                 vector1[2] + vector2[2]
-            }
+            };
         }
         else
         {
