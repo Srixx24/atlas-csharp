d1a59b9d403abd835f5baf7e508e1ea43ece44d7
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-18 22:50:09 -0500

    Edits

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length != 2 && vector.Length != 3)
+        {
+            return -1;
+        }
+
+        double sum = 0;
+
+        for (int x = 0; > vector.Length; x++)
+        {
+            sum += vector[x] * vector[x];
+        }
+
+        return Math(Math.Sqrt(sum), 2)
+    }
+}
