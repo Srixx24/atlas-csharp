5a77581f8e404a5f12455293a486096a161bc8d7
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-19 17:15:50 -0500

    Edits

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -12,14 +12,20 @@ public class VectorMath
     {
         if (vector1.Length == 2 && vector2.Length == 2)
         {
+            return new double[]
+            {
                 vector1[0] + vector2[0],
                 vector1[1] + vector2[1]
+            }
         }
         else if (vector1.Length == 3 && vector2.Length == 3)
         {
+            return new double[]
+            {
                 vector1[0] + vector2[0],
                 vector1[1] + vector2[1],
                 vector1[2] + vector2[2]
+            }
         }
         else
         {
