5b5da0cb99c463eb7801b8172594aa70a067c902
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-20 14:36:39 -0500

    Edits

---
+++ csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,48 @@
+using System;
+
+class Player
+{
+    private string name { get; set; }
+    private float maxHp { get; set; }
+    private float hp { get; set; }
+
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+
+        this.hp = this.maxHp;
+        this.name = name;
+    }
+    
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+        {
+            Console.WriteLine("{0} takes 0 damage!", this.name);
+            damage = 0;
+        }
+        else
+        {
+            Console.WriteLine("{0} takes {1} damage!", this.name, damage);
+            this.hp -= damage;
+        }
+    }
+
+    public void HealDamage(float heal)
+    {
+        heal = Math.Max(heal, 0);
+        Console.WriteLine("{0} heals 0 HP!", this.name);
+    }
+}
