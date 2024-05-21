5a5d15723b655344cd50031163dbd79618049eb4
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-21 10:25:27 -0500

    Edits

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,11 @@
+using System;
+
+/// <summary>
+/// Base class that all other classes will inherit from.
+/// </summary>
+public class BaseClass
+{
+    public string id { get; set; }
+    public DateTime date_created { get; set; }
+    public DateTime date_updated { get; set; }
+}
