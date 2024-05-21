531d914bfffcec44142b747386423b632f1280f7
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-21 17:54:06 -0500

    Edits

---
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,15 @@
+using System;
+
+/// <summary>
+/// Provides methods for matrix mathematics.
+/// </summary>
+public class MatrixMath
+{
+    /// <summary>
+    /// Calculates the inverse of a 2D matrix
+    /// </summary>
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        boop
+    }
+}
