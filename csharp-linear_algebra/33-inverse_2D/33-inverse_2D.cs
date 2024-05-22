e8b761da4bff024f2675360b4c7620ef5f266bf8
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-21 21:55:44 -0500

    Edits

--- csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -18,10 +18,10 @@ public class MatrixMath
         double deter = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
 
         double[,] swapin = new double[2,2];
-        inverse[0, 0] = matrix[1, 1] / deter;
-        inverse[0, 1] = matrix[1, 0] / deter;
-        inverse[1, 0] = matrix[0, 1] / deter;
-        inverse[0, 0] = matrix[0, 0] / deter;
+        swapin[0, 0] = matrix[1, 1] / deter;
+        swapin[0, 1] = matrix[1, 0] / deter;
+        swapin[1, 0] = matrix[0, 1] / deter;
+        swapin[0, 0] = matrix[0, 0] / deter;
 
         return swapin;
     }
