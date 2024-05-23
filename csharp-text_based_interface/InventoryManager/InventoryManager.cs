44a8c4721e7aa52d568b2b82139a8123adbd8d5c
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-05-22 20:56:24 -0500

    Edits

--- csharp-text_based_interface/InventoryManager/InventoryManager.cs
+++ csharp-text_based_interface/InventoryManager/InventoryManager.cs
@@ -1,8 +1,8 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
-using InventoryLibrary;
 using System.Linq;
+using static InventoryLibrary.InventoryObject;
 
 namespace InventoryManager
 {
